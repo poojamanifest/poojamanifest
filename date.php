836b07314183cd43b581f4c84e57ab07cd49a9e7
sourabhmanifest <sourabh@manifestinfotech.com>
2015-05-23 12:24:49 +0530

    commited

---
+++ date.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+  <title>jQuery Datepicker UI Example - Demo Preview</title>
+  <meta name="robots" content="noindex, nofollow"/>
+  
+  <!------------Including jQuery Date UI with CSS-------------->
+  <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="http://code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
+  <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.0/themes/smoothness/jquery-ui.css">
+  
+   <!-- jQuery Code executes on Date Format option ----->
+   <script src="js/script.js"></script>
+   <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="container">
+<h2>jQuery Datepicker UI Example Form</h2>
+<div class="main">
+<form action="" method="post"> 
+
+
+<label>Date Of Birth :</label><br> 
+<input type="text" name="selected_date" id="datepicker"/>
+
+<label>Date Of Birth :</label><br> 
+<input type="text" name="selected_date" id="datepicker1"/>
+ 
+
+
+
+</form> 
+</div>
+</div> 
+</body>
+</html>
